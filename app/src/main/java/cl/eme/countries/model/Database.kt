cea122bce1bcec1d3eff79419f515c4c71f17959
Mauricio Ponce Oyarzún <mauricio.ponce.o@gmail.com>
2021-01-18 12:58:14 -0300

    Refactor -> el converter es una clase separada

--- app/src/main/java/cl/eme/countries/model/Database.kt
+++ app/src/main/java/cl/eme/countries/model/Database.kt
@@ -23,36 +23,6 @@ abstract class CountriesDatabase : RoomDatabase() {
     abstract fun countryDao(): CountryDao
 }
 
-class Converters {
-    @TypeConverter
-    fun list2String(list: List<String>): String {
-        return list.joinToString()
-    }
-
-    @TypeConverter
-    fun string2List(value: String): List<String> {
-        return value.split(",").map {
-            it.trim()
-        }
-    }
-
-    @TypeConverter
-    fun list2Double(list: List<Double>): String {
-        return list.joinToString()
-    }
-
-    @TypeConverter
-    fun doubles2List(value: String): List<Double> {
-        val result = mutableListOf<Double>()
-        value.split(",").forEach {
-            if (!it.isEmpty()) {
-                result.add(it.trim().toDouble())
-            }
-        }
-        return result
-    }
-}
-
 class CountryApplication : Application() {
     companion object {
         var countryDatabase: CountriesDatabase? = null
