bcb085970698efc4413abf06cfa2a312ca5c6ce6
Mauricio Ponce Oyarzún <mauricio.ponce.o@gmail.com>
2021-01-18 10:33:55 -0300

    listado de tareas por realizar. let's go+

--- app/src/main/java/cl/eme/countries/MainActivity.kt
+++ app/src/main/java/cl/eme/countries/MainActivity.kt
@@ -3,6 +3,34 @@ package cl.eme.countries
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+[ ] Consumo de API
+    [ ] Interfaz de operaciones
+    [ ] cliente de retrofit
+    [ ] POJO
+    [ ] dependencias
+    [ ] permiso de internet
+[ ] ROOM
+    [ ] Dao
+    [ ] cliente de base de datos
+    [ ] Entities
+    [ ] dependencias
+    [ ] testing
+    [ ] subclase de application
+    [ ] type converters
+[ ] corutinas (dependencias)
+[ ] Repositorio
+[ ] ViewModel (by viewModels()) -> Implementa el patrón factory
+[ ] Testing a los mappers
+[ ] Fragmento de listado
+    [ ] Layout de item list
+    [ ] Adapter + ViewHolder + RV
+    [ ] imágenes SVG
+[ ] Fragmento de detalle
+[ ] Navigation
+[ ] Testing para la API -> convertir al repo
+*/
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
