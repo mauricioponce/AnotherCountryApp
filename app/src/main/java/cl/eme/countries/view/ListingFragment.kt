779b6a2c85b7f8409b4435d923d0fbd509e4a158
Mauricio Ponce Oyarzún <mauricio.ponce.o@gmail.com>
2021-01-18 15:47:14 -0300

    Se abre el fragmento  de detalle desde el fragmento de listado

--- app/src/main/java/cl/eme/countries/view/ListingFragment.kt
+++ app/src/main/java/cl/eme/countries/view/ListingFragment.kt
@@ -8,7 +8,9 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import cl.eme.countries.MyViewModel
+import cl.eme.countries.R
 import cl.eme.countries.databinding.FragmentListingBinding
+import timber.log.Timber
 
 class ListingFragment: Fragment() {
     private val viewModel: MyViewModel by viewModels()
@@ -30,6 +32,12 @@ class ListingFragment: Fragment() {
             it?.let {
                 adapter.update(it)
             }
+        })
+
+        adapter.selectedItem().observe(viewLifecycleOwner, {
+            Timber.d("item seleccionado es $it")
+
+            activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.main_container, DetailFragment())?.commit()
         })
 
         return binding.root
