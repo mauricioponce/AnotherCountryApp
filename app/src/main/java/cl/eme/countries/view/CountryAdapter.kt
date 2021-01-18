22486c546278a070fa27ca53684328319ec7fe69
Mauricio Ponce Oyarzún <mauricio.ponce.o@gmail.com>
2021-01-18 15:46:25 -0300

    Manejo de onClick en el adapter exponiendo un LiveData llamado selectedItem

--- app/src/main/java/cl/eme/countries/view/CountryAdapter.kt
+++ app/src/main/java/cl/eme/countries/view/CountryAdapter.kt
@@ -2,6 +2,8 @@ package cl.eme.countries.view
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
 import cl.eme.countries.databinding.ItemListBinding
 import cl.eme.countries.model.MinimalCountry
@@ -9,7 +11,11 @@ import coil.load
 
 class CountryAdapter : RecyclerView.Adapter<CountryVH>() {
 
-    var items = listOf<MinimalCountry>()
+    private var items = listOf<MinimalCountry>()
+
+    private val selectedItem = MutableLiveData<MinimalCountry>()
+
+    fun selectedItem(): LiveData<MinimalCountry> = selectedItem
 
     fun update(minimalList: List<MinimalCountry>) {
         items = minimalList
@@ -23,6 +29,9 @@ class CountryAdapter : RecyclerView.Adapter<CountryVH>() {
     override fun onBindViewHolder(holder: CountryVH, position: Int) {
         val item = items[position]
         holder.bind(item)
+        holder.itemView.setOnClickListener {
+            selectedItem.value = item
+        }
     }
 
     override fun getItemCount() = items.size
